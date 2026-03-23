f15f857232ddc60e41b9da8886c296da0b1fe5d2
Tan Tran <tantran@Tans-MacBook-Air.local>
2026-03-23 19:42:20 -0400

    Add js to Projects, make the project cards move

---
+++ javascript/project-card.js
@@ -0,0 +1,26 @@
+const projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.classList.add('hovered');
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.classList.remove('hovered');
+        card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+    });
+
+    card.addEventListener('mousemove', (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const rotateX = ((y - centerY) / rect.height) * 10; // Adjust the multiplier for more/less tilt
+        const rotateY = ((x - centerX) / rect.width) * -10; // Adjust the multiplier for more/less tilt
+
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    }); 
+});
